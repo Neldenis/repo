c1ed06eb5336f2581b9be5f872c0092004d22ffd
Neldenis <ivfnrrf12435@gmail.com>
2024-11-15 14:13:49 +0300

    Update BitOperation_4.c

--- Task1/BitOperation_4.c
+++ Task1/BitOperation_4.c
@@ -1,17 +1,22 @@
 #include "stdio.h"
 int main(){
     
-    int userNumb,userChang,out;
+    int userNumb,userChang,out=0;
     printf("Enter original namber -\t");
     scanf("%d",&userNumb);
     printf("Enter change namber -\t");
     scanf("%d",&userChang);
-    userNumb=userNumb|(userChang<<8);
-    for(int i=1;i<=sizeof(int)*8;++i)
-        printf("%d",out=(userNumb>>(sizeof(int)*8-i)&1));
-    printf("\nNew number - %d",userNumb);
 
+    for(int i=sizeof(int)*8-1;i>=0;--i){
+      if ((i>=16)&&(i<=23)){
+      printf("%d",out=(userChang<<16>>i&1));
+      }
+    else 
+    printf("%d",out=(userNumb>>i&1));
+    } 
+    printf("\n");
 return 0;
 }
 /*4. Поменять в целом положительном числе (типа int) значение третьего байта на
-введенное пользователем число (изначальное число также вводится с клавиатуры) */
+введенное пользователем число (изначальное число также вводится с клавиатуры) 
+*/
